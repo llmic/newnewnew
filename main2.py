96be7c99495d1c3b18fd3f982a2e8f939a120d66
Liuli <2170531272@qq.com>
2025-10-17 22:04:13 +0800

    完善获取当前用户的异步函数，增加JWT解码和用户激活状态验证

--- main2.py
+++ main2.py
@@ -80,5 +80,19 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode
+        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
     except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)],):
+    if current_user.disabled:
+        raise HTTPException(status_code=400,detail="Inactive user")
+    return current_user
